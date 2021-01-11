a35ec455c5a1fc06f31c6e55817531f0527db5fd
Jaroslaw Kurdzieko <jaroslaw.kurdzieko@macrix.pl>
2021-01-11 14:39:07 +0100

    fix userId issue

--- src/Middlink.Core/CQRS/Operations/OperationDto.cs
+++ src/Middlink.Core/CQRS/Operations/OperationDto.cs
@@ -6,7 +6,7 @@ namespace Middlink.Core.CQRS.Operations
     public class OperationDto
     {
         public Guid Id { get; set; }
-        public Guid UserId { get; set; }
+        public string UserId { get; set; }
         public string Name { get; set; }
         public string State { get; set; }
         public string Resource { get; set; }
