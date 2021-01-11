cb70495fb20ddead0621cbd5874f80ad7c365547
Adam Nowak <an@macrix.pl>
2021-01-11 20:07:26 +0100

    Add next version of Bind method.

--- src/Middlink.Core/Extensions/Extensions.cs
+++ src/Middlink.Core/Extensions/Extensions.cs
@@ -7,10 +7,20 @@ namespace Middlink.Core.Extensions
 {
     public static partial class Extensions
     {
+        public static Guid ToGuid(this string from)
+        {
+            Guid result;
+            Guid.TryParse(from, out result);
+            return result;
+        }
+
         public static string Underscore(this string value) => string.Concat(value.Select((x, i) => i > 0 && char.IsUpper(x) ? "_" + x.ToString() : x.ToString()));
         public static T Bind<T>(this T model, Expression<Func<T, object>> expression, object value) => model.Bind<T, object>(expression, value);
 
         public static T BindId<T>(this T model, Expression<Func<T, Guid>> expression) => model.Bind(expression, Guid.NewGuid());
+
+        public static T Bind<T, TFrom, TTo>(this T model, Expression<Func<T, TTo>> expression, TFrom value, Func<TFrom, TTo> converter)
+            => model.Bind<T, TTo>(expression, converter(value));
 
         private static TModel Bind<TModel, TProperty>(this TModel model, Expression<Func<TModel, TProperty>> expression, object value)
         {
