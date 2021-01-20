f89367685646f6501f09769af136353793ca28bd
Jaroslaw Kurdzieko <jaroslaw.kurdzieko@macrix.pl>
2021-01-20 15:14:08 +0100

    expose operation ioc mechanism

---
+++ src/CQRS/Middlink.CQRS.Operations.SignalR/Extensions/ServiceExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Middlink.Core.CQRS.Handlers;
+using Middlink.Core.CQRS.Operations;
+using Middlink.CQRS.Operations.SignalR.Services.Operations;
+using System;
+
+namespace Middlink.CQRS.Operations.SignalR.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void AddOperations(this IServiceCollection builder)
+        {
+            builder.AddTransient<IOperationPublisher, OperationPublisher>();
+            builder.AddTransient<IOperationsStorage, OperationsStorage>();
+            builder.AddTransient(typeof(IEventHandler<>), typeof(OperationEventHandler<>));
+        }
+    }
+}
