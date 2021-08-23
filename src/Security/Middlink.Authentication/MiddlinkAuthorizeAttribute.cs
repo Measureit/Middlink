ad9196f0d7c82c696f3358b7c5514bb7272287f2
Jaroslaw Kurdzieko <jaroslaw.kurdzieko@macrix.pl>
2021-08-23 19:06:31 +0200

    add library for permission system customization

---
+++ src/Security/Middlink.Authentication/MiddlinkAuthorizeAttribute.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Middlink.Authentication
+{
+    public class MiddlinkAuthorizeAttribute : AuthorizeAttribute
+    {
+        internal const string POLICY_PREFIX = "Middlink";
+
+        public MiddlinkAuthorizeAttribute(string permission) => Permission = permission;
+
+        public string Permission
+        {
+            get
+            {
+                return Policy.Substring(POLICY_PREFIX.Length);
+            }
+            set
+            {
+                Policy = $"{POLICY_PREFIX}{value}";
+            }
+        }
+    }
+}
