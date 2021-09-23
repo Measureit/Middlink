da0a1b97b95950af940a889a182c544563b07eff
Jaroslaw Kurdzieko <jaroslaw.kurdzieko@macrix.pl>
2021-09-23 08:31:41 +0200

    add the anonymous command mechansim

---
+++ src/Middlink.Core/CQRS/Commands/IAnonymousCommand.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Middlink.Core.CQRS.Commands
+{
+    public interface IAnonymousCommand : ICommand
+    {
+        Guid SessionId { get; set; }
+    }
+
+    public interface IAnonymousCommand<TKey> : ICommand<TKey>, IAnonymousCommand
+    {
+    }
+}
