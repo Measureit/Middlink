2a349d9b0b15938798f827d98885c27e9c8a0e81
Jaroslaw Kurdzieko <jaroslaw.kurdzieko@macrix.pl>
2021-09-24 15:45:57 +0200

    fix anonymouse command parameter accesor

--- src/Middlink.Core/CQRS/Commands/IAnonymousCommand.cs
+++ src/Middlink.Core/CQRS/Commands/IAnonymousCommand.cs
@@ -4,7 +4,7 @@ namespace Middlink.Core.CQRS.Commands
 {
     public interface IAnonymousCommand : ICommand
     {
-        Guid SessionId { get; set; }
+        Guid SessionId { get; }
     }
 
     public interface IAnonymousCommand<TKey> : ICommand<TKey>, IAnonymousCommand
